7590f0ddf27d397f42e7a388461b3f3b41a1ec00
halimfathin7 <halimfathin7@gmail.com>
2025-03-20 13:22:00 +0000

    custom cursor

--- src/main.jsx
+++ src/main.jsx
@@ -15,6 +15,33 @@ import Navbar from "./Navbar.jsx";
 /* eslint-disable react/prop-types */
 
 function App() {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [isHovering, setIsHovering] = useState(false);
+  const [scrollOffset, setScrollOffset] = useState(0);
+
+  useEffect(() => {
+    // Event delegation untuk hover pada link
+    const handleMouseOver = (e) => {
+      if (e.target.matches("a, h1, h3, h4, strong, #Name")) {
+        setIsHovering(true);
+      }
+    };
+
+    const handleMouseOut = (e) => {
+      if (e.target.matches("a, h1, h3, h4, strong, #Name")) {
+        setIsHovering(false);
+      }
+    };
+
+    document.addEventListener("mouseover", handleMouseOver);
+    document.addEventListener("mouseout", handleMouseOut);
+
+    return () => {
+      document.removeEventListener("mouseover", handleMouseOver);
+      document.removeEventListener("mouseout", handleMouseOut);
+    };
+  }, []);
+
   useEffect(() => {
     Aos.init({ duration: 500, once: false });
 
@@ -24,7 +51,11 @@ function App() {
       smartphone: { smooth: false },
       multiplier: 2,
     });
-  
+
+    scroll.on("scroll", (instance) => {
+      setScrollOffset(instance.scroll.y); // Simpan posisi scroll
+    });
+
     const scrollContainer = document.querySelector("[data-scroll-container]");
     const resizeObserver = new ResizeObserver(() => scroll.update());
     resizeObserver.observe(scrollContainer);
@@ -35,11 +66,37 @@ function App() {
     };
   }, []);
 
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      setPosition({ x: e.clientX, y: e.clientY + scrollOffset }); // Sesuaikan posisi dengan scroll
+    };
+
+    const scrollContainer = document.querySelector("[data-scroll-container]");
+    if (scrollContainer) {
+      scrollContainer.addEventListener("mousemove", handleMouseMove);
+    }
+
+    return () => {
+      if (scrollContainer) {
+        scrollContainer.removeEventListener("mousemove", handleMouseMove);
+      }
+    };
+  }, [scrollOffset]);
+
   return (
+    
     <div  data-scroll-container>
+            <div
+      className={`custom-cursor ${isHovering ? "custom-cursor--link" : ""}`}
+      style={{
+        left: `${position.x}px`,
+        top: `${position.y}px`,
+      }}
+    />  
       <Header />
       <div className='px-2 mx-auto p-0 mt-8 container'>
-      <Navbar />
+    
+        <Navbar />
         <Intro />
         <div className='parallax'>
           <Service />
