0f55cb0ae414ce58587f1a4bc59eac09ee39570b
M.Fathin Halim <halimfathin7@gmail.com>
2025-04-20 10:01:10 +0700

    remove animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,20 +13,6 @@ export default {
    },   
   theme: {
     extend: {
-      animation: {
-        scroll: 'scroll 40s linear infinite',
-        scrollReverse: 'scrollReverse 40s linear infinite',
-      },
-      keyframes: {
-        scroll: {
-          '0%': { transform: 'translateX(0)' },
-          '100%': { transform: 'translateX(-50%)' },
-        },
-        scrollReverse: {
-          '0%': { transform: 'translateX(-50%)' },
-          '100%': { transform: 'translateX(0)' },
-        },
-      },
     },
   },
   plugins: [],
